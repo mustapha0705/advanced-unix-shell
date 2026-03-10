3d01bc983933f839512acfee63cc5b042bc8a04d
Mustapha Moahmmed <mustaphamohammed0705@gmail.com>
2026-03-09 22:18:11 -0400

    Separate execute functionality

--- shell.c
+++ shell.c
@@ -60,53 +60,6 @@ int main(int argc, char **argv)
     return (0);
 }
 
-int _execute(char **command)
-{
-    char *full_cmd;
-    int status = 0;
-
-    pid_t pid;
-
-    full_cmd = _getpath(command[0]);
-    if (!full_cmd)
-    {
-        return 127;
-    }
-
-    pid = fork();
-
-    if (pid < 0)
-    {
-        perror("fork");
-        return (-1);
-    }
-
-    if (pid == 0)
-    {
-        int val = execve(full_cmd, command, environ);
-        if (val == -1)
-        {
-            perror("execve");
-            exit(EXIT_FAILURE);
-        }
-
-        return (status);
-    }
-
-    else
-    {
-        if (waitpid(pid, &status, 0) == -1)
-        {
-            perror("waitpid");
-            free(full_cmd);
-            return -1;
-        }
-    }
-    free(full_cmd);
-
-    return (status);
-}
-
 void free2Darray(char **array)
 {
     int i;
